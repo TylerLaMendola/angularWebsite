686111d34a84a8f56b9deb15f9a7518240581710
Tyler LaMendola <tylerlam@umich.edu>
2020-07-02 19:58:50 -0400

    initial

---
+++ angularWebsite/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeWrapperComponent } from './components/home-wrapper/home-wrapper.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeWrapperComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
